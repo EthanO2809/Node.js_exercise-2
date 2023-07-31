176871decd40244eb4fbbbff6afb788b11ddd01b
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-07-31 16:13:31 +0200

    exercise-2 complete

---
+++ Index.js
@@ -0,0 +1,9 @@
+//Exercise 2
+const p1 = require('./Persons.js')
+console.log('laiquah', 'cotie', 'cotielaiquah@gmail.com');
+
+const p2 = require('./Persons.js')
+console.log('ganiefah', 'majiet', 'gmajiet180804@gmail.com');
+
+const p3 = require('./Persons.js')
+console.log('laeeq', 'majal', 'laeeqmajal@gmail.com');
---
+++ Persons.js
@@ -0,0 +1,7 @@
+const Person1 = (firstName, lastName, emailAddress) => {
+    this.name = firstName,
+    this.surname = lastName,
+    this.email = emailAddress
+};
+
+module.exports = Person1;
